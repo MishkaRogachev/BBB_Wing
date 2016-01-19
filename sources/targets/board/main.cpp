f0bf005198105ff8e538a6d9d3c8fbf0f5377aa2
rogachev <mishkarogachev@gmail.com>
2016-01-19 10:20:10 +0300

    Added forgotten init() in SensorAltimeterNode

--- sources/targets/board/main.cpp
+++ sources/targets/board/main.cpp
@@ -12,6 +12,7 @@ int main(int argc, char* argv[])
     QCoreApplication app(argc, argv);
 
     domain::SensorAltimeterNode altNode;
+    altNode.init();
     altNode.start(25);
 
     return app.exec();
