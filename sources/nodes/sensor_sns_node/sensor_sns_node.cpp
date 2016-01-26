74358cafeebf3acfb1df91b47fed76ad8f4db8c2
rogachev <mishkarogachev@gmail.com>
2016-01-26 11:24:48 +0300

    Little more SensorSnsNode code. Now node pubish main gpsd data. Hope it is correct

--- sources/nodes/sensor_sns_node/sensor_sns_node.cpp
+++ sources/nodes/sensor_sns_node/sensor_sns_node.cpp
@@ -37,7 +37,7 @@ SensorSnsNode::~SensorSnsNode()
 
 void SensorSnsNode::init()
 {
-
+    d->handle.stream(WATCH_ENABLE | WATCH_JSON);
 }
 
 void SensorSnsNode::exec()
@@ -47,8 +47,29 @@ void SensorSnsNode::exec()
     if ((data = d->handle.read()) == nullptr)
     {
         d->pub.publish("status", QByteArray::number(false));
+        this->init();
         return;
     }
 
     d->pub.publish("status", QByteArray::number(true));
+
+    d->pub.publish("fix", QByteArray::number(data->status));
+    d->pub.publish("satellites_used", QByteArray::number(data->satellites_used));
+    d->pub.publish("satellites_visible", QByteArray::number(data->satellites_visible));
+
+    if (!data->status) return;
+
+    d->pub.publish("fix_mode", QByteArray::number(data->fix.mode));
+
+    if (data->fix.mode < 2) return;
+
+    d->pub.publish("latitude", QByteArray::number(data->fix.latitude));
+    d->pub.publish("longitude", QByteArray::number(data->fix.longitude));
+    d->pub.publish("course", QByteArray::number(data->fix.track));
+    d->pub.publish("velocity", QByteArray::number(data->fix.speed));
+
+    if (data->fix.mode < 3) return;
+
+    d->pub.publish("altitude", QByteArray::number(data->fix.altitude));
+    d->pub.publish("climb", QByteArray::number(data->fix.climb));
 }
