de840c3baf790741dfe028c6fb193273fa43c0a9
rogachev <mishkarogachev@gmail.com>
2016-01-27 14:51:00 +0300

    FlightRecorderNode launch added with freq 2 Hz

--- sources/targets/board/main.cpp
+++ sources/targets/board/main.cpp
@@ -6,6 +6,7 @@
 #include "debug_node.h"
 #include "sensor_altimeter_node.h"
 #include "sensor_sns_node.h"
+#include "flight_recorder_node.h"
 
 int main(int argc, char* argv[])
 {
@@ -14,14 +15,17 @@ int main(int argc, char* argv[])
     domain::DebugNode debugNode;
     domain::SensorAltimeterNode altimeterNode;
     domain::SensorSnsNode snsNode;
+    domain::FlightRecorderNode flightRecorderNode;
 
     debugNode.init();
     altimeterNode.init();
     snsNode.init();
+    flightRecorderNode.init();
 
     debugNode.start(1);
     altimeterNode.start(2);
     snsNode.start(10);
+    flightRecorderNode.start(2);
 
     return app.exec();
 }
