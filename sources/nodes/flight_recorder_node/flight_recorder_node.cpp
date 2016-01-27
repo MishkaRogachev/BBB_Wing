6b9cbcf0624ed345855df403eaceab4e239c7bb8
rogachev <mishkarogachev@gmail.com>
2016-01-27 14:41:45 +0300

    Basic FlightRecorderNode implimentation

---
+++ sources/nodes/flight_recorder_node/flight_recorder_node.cpp
@@ -0,0 +1,79 @@
+#include "flight_recorder_node.h"
+
+// Qt
+#include <QDebug>
+#include <QDir>
+#include <QFile>
+#include <QDateTime>
+
+// Internal
+#include "subscriber.h"
+
+namespace
+{
+    const char* delimiter = ",";
+    const QStringList topics = {
+        "time_stamp",
+        "alt_status", "alt_altitude", "alt_temperature",
+        "sns_status", "sns_status", "sns_fix", "sns_fix_mode",
+        "sns_latitude", "sns_longitude", "sns_course", "sns_velocity",
+        "sns_altitude", "sns_climb" };
+}
+
+using namespace domain;
+
+class FlightRecorderNode::Impl
+{
+public:
+    Subscriber sub;
+    QFile file;
+    QMap<QString, QByteArray> messages;
+};
+
+FlightRecorderNode::FlightRecorderNode(QObject* parent):
+    AbstractNodeFrequency(parent),
+    d(new Impl())
+{}
+
+FlightRecorderNode::~FlightRecorderNode()
+{
+    delete d;
+}
+
+void FlightRecorderNode::init()
+{
+     d->sub.connectTo("ipc://altimeter");
+     d->sub.connectTo("ipc://sns");
+
+     d->sub.subscribe("");
+     connect(&d->sub, &Subscriber::received, this, &FlightRecorderNode::onReceived);
+}
+
+void FlightRecorderNode::exec()
+{
+    QTextStream stream(&d->file);
+    if (!d->file.isOpen())
+    {
+        d->file.setFileName("/media/card/" +
+                            QDateTime::currentDateTime().toString(
+                                "dd.MM.yyyy_hh:mm:ss") + ".csv");
+        bool exists = d->file.exists();
+        if (!d->file.open(QIODevice::Append | QIODevice::Text))
+            return;
+        if (!exists) stream << ::topics.join(::delimiter) << endl;
+    }
+
+    d->messages["time_stamp"] = QTime::currentTime().toString(
+                                    "hh:mm:ss.zzz").toLatin1();
+    QList<QByteArray> buffer;
+    for (const QString& topic: ::topics)
+        buffer.append(d->messages.value(topic));
+
+    stream << buffer.join(::delimiter) << endl;
+    stream.flush();
+}
+
+void FlightRecorderNode::onReceived(const QString& topic, const QByteArray& msg)
+{
+    d->messages[topic] = msg;
+}
---
+++ sources/nodes/flight_recorder_node/flight_recorder_node.h
@@ -0,0 +1,26 @@
+#ifndef FLIGHT_RECORDER_NODE_H
+#define FLIGHT_RECORDER_NODE_H
+
+#include "abstract_node_frequency.h"
+
+namespace domain
+{
+    class FlightRecorderNode: public AbstractNodeFrequency
+    {
+    public:
+        FlightRecorderNode(QObject* parent = nullptr);
+        ~FlightRecorderNode() override;
+
+        void init() override;
+        void exec() override;
+
+    private slots:
+        void onReceived(const QString& topic, const QByteArray& msg);
+
+    private:
+        class Impl;
+        Impl* const d;
+    };
+}
+
+#endif // FLIGHT_RECORDER_NODE_H
