b584ca197f79cfb09a2e5ae220e88df7edaca99c
rogachev <mishkarogachev@gmail.com>
2016-04-21 16:56:05 +0300

    Pca9685 moved to ServoControlNode

---
+++ sources/nodes/servo_control_node/servo_controllers/i_servo_controller.h
@@ -0,0 +1,14 @@
+#ifndef I_SERVO_CONTROLLER_H
+#define I_SERVO_CONTROLLER_H
+
+namespace devices
+{
+    class IServoController
+    {
+        virtual IServoController() {}
+
+        // TODO: ServoControllers interface
+    };
+}
+
+#endif // I_SERVO_CONTROLLER_H
