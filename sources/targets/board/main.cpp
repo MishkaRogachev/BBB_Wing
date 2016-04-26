db4b7b268e3df0f9656545bb139eb58d7f28c31b
MishkaRogachev <mishkarogachev@gmail.com>
2016-04-26 14:12:56 +0300

    Renaming: ServoControlNode > ServoDrivesNode

--- sources/targets/board/main.cpp
+++ sources/targets/board/main.cpp
@@ -8,13 +8,13 @@
 #include "sensor_altimeter_node.h"
 #include "sensor_ins_node.h"
 #include "sensor_sns_node.h"
+#include "servo_drives_node.h"
 
 #include "failures_handler_node.h"
 #include "flight_recorder_node.h"
 
 #include "flight_navigator_node.h"
 #include "flight_controller_node.h"
-#include "servo_control_node.h"
 
 #include "board_gateway_node.h"
 
@@ -29,13 +29,13 @@ int main(int argc, char* argv[])
     scheduler.addNode(new SensorAltimeterNode());
     scheduler.addNode(new SensorInsNode());
     scheduler.addNode(new SensorSnsNode());
+    scheduler.addNode(new ServoDrivesNode());
 
     scheduler.addNode(new FailuresHandlerNode());
     scheduler.addNode(new FlightRecorderNode());
 
     scheduler.addNode(new FlightNavigatorNode());
     scheduler.addNode(new FlightControllerNode());
-    scheduler.addNode(new SerovoControlNode());
 
     scheduler.addNode(new BoardGatewayNode());
 
