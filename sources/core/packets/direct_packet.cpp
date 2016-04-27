c39c07caa4057154852f1db32cd53d292bf0d476
MishkaRogachev <mishkarogachev@gmail.com>
2016-04-27 17:46:10 +0300

    Direct packet realization

--- sources/core/packets/direct_packet.cpp
+++ sources/core/packets/direct_packet.cpp
@@ -1,6 +1,29 @@
 #include "direct_packet.h"
 
-DirectPacket::DirectPacket()
-{
+using namespace domain;
 
+QDataStream& DirectPacket::operator >>(QDataStream& stream) const
+{
+    stream << manual.deviationX;
+    stream << manual.deviationY;
+    stream << manual.deviationZ;
+
+    return stream;
+}
+
+QDataStream& DirectPacket::operator <<(QDataStream& stream)
+{
+    stream >> manual.deviationX;
+    stream >> manual.deviationY;
+    stream >> manual.deviationZ;
+
+    return stream;
+}
+
+DirectPacket DirectPacket::fromByteArray(const QByteArray& data)
+{
+    QDataStream stream(data);
+    DirectPacket packet;
+    stream >> packet;
+    return packet;
 }
--- sources/core/packets/direct_packet.h
+++ sources/core/packets/direct_packet.h
@@ -1,11 +1,27 @@
 #ifndef DIRECTPACKET_H
 #define DIRECTPACKET_H
 
+#include "packet.h"
 
-class DirectPacket : public Packet
+namespace domain
 {
-public:
-    DirectPacket();
-};
+    class DirectPacket : public Packet
+    {
+    public:
+        // bool isManual; TODO: manual/automatic
+
+        struct Manual
+        {
+            float deviationX = 0.0;
+            float deviationY = 0.0;
+            float deviationZ = 0.0;
+        } manual;
+
+        QDataStream& operator >>(QDataStream& stream) const override;
+        QDataStream& operator <<(QDataStream& stream) override;
+
+        static DirectPacket fromByteArray(const QByteArray& data);
+    };
+}
 
 #endif // DIRECTPACKET_H
