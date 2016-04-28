f989b71d41824be21177fa8799b205ae60d87b9d
MishkaRogachev <mishkarogachev@gmail.com>
2016-04-28 16:11:14 +0300

    FlightControllerNode: direct packet to drive impacts

--- sources/nodes/flight_controller_node/flight_controller_node.cpp
+++ sources/nodes/flight_controller_node/flight_controller_node.cpp
@@ -6,8 +6,12 @@
 // Internal
 #include "config.h"
 #include "core.h"
+
 #include "subscriber.h"
 #include "publisher.h"
+
+#include "direct_packet.h"
+#include "drive_impacts_packet.h"
 
 using namespace domain;
 
@@ -16,6 +20,8 @@ class FlightControllerNode::Impl
 public:
     Subscriber sub;
     Publisher pub;
+
+    DriveImpacts impacts;
 };
 
 FlightControllerNode::FlightControllerNode(QObject* parent):
@@ -39,18 +45,29 @@ void FlightControllerNode::init()
                        endpoints::navigator,
                        endpoints::boardGateway });
 
-    d->sub.subscribe(topics::data);
+    d->sub.subscribe({ topics::data });
     connect(&d->sub, &Subscriber::received, this,
             &FlightControllerNode::onSubReceived);
 }
 
 void FlightControllerNode::exec()
 {
-    // TODO: FlightControllerNode::exec
+    DriveImpactsPacket packet;
+    packet.impacts = d->impacts;
+    d->pub.publish(topics::driveImpactsPacket, packet.toByteArray());
 }
 
 void FlightControllerNode::onSubReceived(const QString& topic,
-                                       const QByteArray& msg)
+                                         const QByteArray& msg)
 {
-    // TODO: FlightControllerNode::onSubReceived
+    if (topic == topics::directPacket)
+    {
+        DirectPacket packet = DirectPacket::fromByteArray(msg);
+
+        // TODO: deviation to angle recalculation
+        d->impacts[0] = packet.manual.deviationX;
+        d->impacts[1] = packet.manual.deviationY;
+        d->impacts[2] = packet.manual.deviationZ;
+    }
+
 }
